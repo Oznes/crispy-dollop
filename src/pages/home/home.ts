b7d1bc063d9971e9ba2c8874861eacc92d8c8573
Senzo <snzpthn@gmail.com>
2018-06-27 09:45:28 +0200

    first commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,8 +7,42 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  teamA: number;
+  teamB: number;
 
+  constructor(public navCtrl: NavController) {
+    this.teamA= 0;
+    this.teamB = 0;
+  }
+  onReset(){
+    this.teamA=0;
+    this.teamB=0;
+
+  }
+
+  //FREE THROW
+  onFreeB(){
+    this.teamB += 1;
+  }
+  onFreeA(){
+    this.teamA += 1;
+  }
+
+    //ON THREE
+  onThreeB(){
+    this.teamB += 3;
+  }
+  onThreeA(){
+    this.teamA += 3;
+  }
+
+  //ON TWO
+  onTwoB(){
+    this.teamB += 2;
+  }
+  
+  onTwoA(){
+    this.teamA += 2;
   }
 
 }
